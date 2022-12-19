09b4e6657ea265aea3a7fd1d6429d581f78c4e73
kenny robert <skenrobert@gmail.com>
2022-12-19 17:52:36 -0500

    data picker funciona

--- build/js/url.js
+++ build/js/url.js
@@ -1,3 +1,4 @@
+const url_img = "http://localhost:85/miggo-accountant-back/public/images/img_api/";
 const url_back = "http://localhost:85/miggo-accountant-back/public/api/";
 const url_front = "http://localhost:85/miggo-front/pages/";
 const home = "http://localhost:85/miggo-front/";
