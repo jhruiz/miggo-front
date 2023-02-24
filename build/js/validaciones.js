2d572ae15e27810deadc652747b2b306d64cfc80
kenny robert <skenrobert@gmail.com>
2023-02-24 17:44:17 -0500

    update fechas

--- build/js/validaciones.js
+++ build/js/validaciones.js
@@ -139,3 +139,17 @@ function calcularDigito(){
 
     }
 }
+
+
+//************************************************************************************************************** */
+
+moment.locale("es", {
+  days: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+  daysShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+  daysMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+  months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+  monthsShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+  today: "Hoy",
+});
+
+//******************************************************************************************************************* */
